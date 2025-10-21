e388983117cbf7aa72328e945620b4f18bed17f7
abhijithpr123 <abhipr222@gmail.com>
2025-10-21 16:44:45 +0530

    todo using while is completed

--- js/index.js
+++ js/index.js
@@ -2,26 +2,46 @@ let Todo = [];
 
 while(true){
     let input = prompt("[1] Add Task \n[2] Show Tasks \n[3] Edit task \n[4] Delete task \n[5] Exit")
-    if(input=="1"){
+    //Add task
+    if(input==="1"){
         let task = prompt("Add Task");
         Todo.push(task)
     }
-    else if (input=="2"){
+    //Display tasks
+    else if (input==="2"){
         let str=``
-        for(let i = 0;i<Todo.length;i++){
-            str += `[${i+1}] ${Todo[i]}\n` 
+        // for(let i = 0;i<Todo.length;i++){
+        //     str += `[${i+1}] ${Todo[i]}\n` 
+        // }
+        for(const[index,tsk] of Todo.entries()){
+            str+=`[${index+1}] ${tsk} \n`
         }
         alert(str)
     }
-    else if(input=="3"){
-        
+    //Edit task
+    else if(input==="3"){
+        let str=``
+        for(const[index,tsk] of Todo.entries()){
+            str+=`[${index+1}] ${tsk} \n`
+        }
+        let edit = prompt(`${str} \nEnter id number and new task name like 'id,new_task(3,drink)'...!`).
+        split(",");
+        Todo[edit[0]-1]=edit[1]
     }
-    else if(input=="4"){
-        
+    //Delete task
+    else if(input==="4"){
+        let str=``
+        for(const[index,tsk] of Todo.entries()){
+            str+=`[${index+1}] ${tsk} \n`
+        }
+        let del =prompt(`${str} \nEnter remove id of task`)
+        Todo.splice(del-1,1);
     }
-    else if (input=="5"){
+    //Exit
+    else if (input==="5"){
         break;
     }
+    //Error message
     else{
         alert("Please enter listed numbers")
     }
